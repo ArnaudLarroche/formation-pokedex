32b07154b62ed64ccbaad04f2dc8d03cdaeba057
Arnaud Larroche <arnaudl@theodo.fr>
2018-04-06 11:12:55 +0200

    Add Pokemon list in App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import './App.css';
 import Pokemon from './components/Pokemon/Pokemon.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemon: {}
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			pokemon: {}
+		}
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-	<Pokemon id="12" />
-      </div>
-    );
-  }
+	render() {
+		const pokemonIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+		const pokemonList = pokemonIds.map((id, key) => <Pokemon key={key} id={id} />);
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				{pokemonList}
+			</div>
+		);
+	}
 
 }
 
