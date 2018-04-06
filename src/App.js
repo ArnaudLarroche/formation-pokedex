d3bfd744ca0ef7eede755ab47ef47531eefcbdae
Arnaud Larroche <arnaudl@theodo.fr>
2018-04-06 10:01:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: {}
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+	  The best pokemon is {this.state.pokemon.name} !
+        </p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+      .then(response => response.json())
+      .then(responseJson => { console.log(responseJson); return responseJson; })
+      .then(responseJson => this.setState({
+        pokemon: responseJson
+      }));
+  }
+}
+
+export default App;
